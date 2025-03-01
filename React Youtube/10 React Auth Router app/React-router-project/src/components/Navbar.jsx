0aead8732297092a8cac6fe5a313c9f468ffb67b
jay kumar <kumarprince13833@gmail.com>
2025-03-02 01:35:42 +0530

    bugs

--- React Youtube/10 React Auth Router app/React-router-project/src/components/Navbar.jsx
+++ React Youtube/10 React Auth Router app/React-router-project/src/components/Navbar.jsx
@@ -6,34 +6,13 @@ import LOGOO from "../assets/LOGOO.png";
 
 function Navbar({ loggedIn, setLoggedIn }) {
   return (
-<<<<<<< HEAD:Youtube/Day 10/React-router-project/src/components/Navbar.jsx
-<<<<<<<< HEAD:Frontend Development/Frontend Development/Youtube/10 React Auth Router app/React-router-project/src/components/Navbar.jsx
     <div className="flex text-richblack-100  items-center w-11/12 max-w-[1160px] py-4 mx-auto  justify-between">
       <Link to="/">
         <img className="mt-5" src={LOGOO} alt="Logo" width={160} height={32} loading="lazy"></img>
-========
-    <div className="flex justify-evenly">
-      <Link to="/">
-        <img src={logo} alt="Logo" width={160} height={32} loading="lazy"></img>
-        {/* //  loading="lazy" means */}
->>>>>>>> 769756be8c28ec382c68ff2e5e85d1999f55a032:Youtube/Day 10/React-router-project/src/components/Navbar.jsx
-=======
-    <div className="flex text-richblack-100  items-center w-11/12 max-w-[1160px] py-4 mx-auto  justify-between">
-      <Link to="/">
-        <img className="mt-5" src={LOGOO} alt="Logo" width={160} height={32} loading="lazy"></img>
->>>>>>> eff30f4 (Fixed embedded repo issue):Youtube/10 React Auth Router app/React-router-project/src/components/Navbar.jsx
       </Link>
 
       <nav>
-<<<<<<< HEAD:Youtube/Day 10/React-router-project/src/components/Navbar.jsx
-<<<<<<<< HEAD:Frontend Development/Frontend Development/Youtube/10 React Auth Router app/React-router-project/src/components/Navbar.jsx
         <ul className="flex gap-x-6">
-========
-        <ul className="flex gap-3">
->>>>>>>> 769756be8c28ec382c68ff2e5e85d1999f55a032:Youtube/Day 10/React-router-project/src/components/Navbar.jsx
-=======
-        <ul className="flex gap-x-6">
->>>>>>> eff30f4 (Fixed embedded repo issue):Youtube/10 React Auth Router app/React-router-project/src/components/Navbar.jsx
           <li>
             <Link to="/">Home</Link>
           </li>
@@ -46,22 +25,9 @@ function Navbar({ loggedIn, setLoggedIn }) {
         </ul>
       </nav>
 
-<<<<<<< HEAD:Youtube/Day 10/React-router-project/src/components/Navbar.jsx
-<<<<<<<< HEAD:Frontend Development/Frontend Development/Youtube/10 React Auth Router app/React-router-project/src/components/Navbar.jsx
 
     {/* login signup buttons */}
       <div className="flex gap-4 items-center ">
-========
-      {/* Buttons --> Login , Singup, Logout Dashboard
-          If !loggedIn is true, the right-hand side (<Link>...</Link>) renders.
-          If !loggedIn is false, nothing is rendered. */}
-      <div className="flex ml-3 gap-3">
->>>>>>>> 769756be8c28ec382c68ff2e5e85d1999f55a032:Youtube/Day 10/React-router-project/src/components/Navbar.jsx
-=======
-
-    {/* login signup buttons */}
-      <div className="flex gap-4 items-center ">
->>>>>>> eff30f4 (Fixed embedded repo issue):Youtube/10 React Auth Router app/React-router-project/src/components/Navbar.jsx
         {!loggedIn && (
           <Link to="/login">
             <button className="cursor-pointer bg-richblack-800 py-[8px] px-[12px] rounded-[8px] border border-richblack-700">Login</button>
